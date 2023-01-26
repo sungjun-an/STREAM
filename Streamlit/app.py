bf00d66ea48d75718e122002a0d50707ab15e737
sungjun-an <hiahn777@naver.com>
2023-01-26 17:03:19 +0900

    CIFAR-10 이미지 분류모델 탑

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import plotly.express as px
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+
+global img;
 
 def image_resize(image):
-    return image.resize((224,224))
+    return image.resize((32,32))
 
 def image_rotate(image):
     return image.transpose(Image.ROTATE_180)
@@ -39,9 +42,9 @@ if submitted:
     st.plotly_chart(line_fig)
 
 file = st.file_uploader("이미지 선택", type=['png'])
+img = Image.open(file)
 
 if file is not None:
-    img = Image.open(file)
     st.image(file)
 
 img_resize=st.checkbox(label="이미지 크기")
@@ -57,3 +60,14 @@ if submite:
     if img_rotation:
         img = image_rotate(img)
     st.image(img)
+    print(np.shape(img))
+    new_model = tf.keras.models.load_model('model.h5')
+    labels = ['비행기', '자동차', '새', '고양이', '사슴', '개', '개구리', '말', '배', '트럭']
+    width = 32
+    height = 32
+    channel = 3
+    print(np.shape(img))
+    img = np.array(img)
+    x_test = img.astype('float32') / 255.0
+    y_prob = new_model.predict(x_test.reshape(1,width, height, channel), verbose=0)
+    st.write(labels[np.argmax(y_prob)])
